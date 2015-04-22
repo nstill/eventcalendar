55bb63f368f829a4bde89dd5775bf26317eb4bca
nstill <nalani.still@gmail.com>
2015-04-22 09:16:44 +0300

    Update and rename calendarscript to calendar.js

---
+++ calendar.js
@@ -0,0 +1,7 @@
+function addEvent (){
+	var eventName = document.forms["eventCalendar"]["eventName"].value;
+	var eventDate = document.forms["eventCalendar"]["eventDate"].value;
+	var eventTime = document.forms["eventCalendar"]["eventTime"].value;
+	var eventLocation = document.forms ["eventCalendar"]["eventLocation"].value;
+	alert("Event Added!");
+}
